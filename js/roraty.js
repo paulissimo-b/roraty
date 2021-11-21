3d9ef5742b38931f05054699980fe82ceeed0258
Paweł <p.bartela@gmail.com>
2021-11-21 11:57:52 +0100

    Modificated cusor (to pointed) on image (slide 2), added candles on start slide

--- js/roraty.js
+++ js/roraty.js
@@ -1,8 +1,10 @@
 let img = document.querySelector('#sportsman-img');
+let imgP = document.querySelector('.img-box p');
 
 
 function log() {
-    img.setAttribute('style', 'transition: filter 1s linear; filter: blur(0);');
+    img.setAttribute('style', 'transition: filter 1s linear; filter: blur(0); cursor: default;');
+    imgP.setAttribute('style', 'font-size: 2rem;');
 }
 
 img.addEventListener('click', log);
