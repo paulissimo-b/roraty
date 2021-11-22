9a3559f9414de7f8d2ef1c153e1ddfb8d9dd3364
Paweł <p.bartela@gmail.com>
2021-11-22 21:23:09 +0100

    Added video-box for film of the day, made smaller version of images, some css corrections

--- js/roraty.js
+++ js/roraty.js
@@ -1,8 +1,24 @@
 let img = document.querySelector('#sportsman-img');
+let showVideo = document.querySelector('.show-video');
+let closeVideo = document.querySelector('.video-box p');
+let videoBox = document.querySelector('.video-box')
 
 
-function log() {
+function setAttr() {
     img.setAttribute('style', 'transition: filter 1s linear; filter: blur(0); cursor: default;');
+    console.log('tutaj');
 }
 
-img.addEventListener('click', log);
+function close() {
+    videoBox.setAttribute('style', 'transition: height .9s ease-in-out, box-shadow .1s .9s; height: 0; box-shadow: none;');
+    closeVideo.setAttribute('style', 'display: none;');
+} 
+
+function show() {
+    videoBox.setAttribute('style', 'transition: height .9s ease-in-out, box-shadow .1s; height: 80%; box-shadow: 0 0 600px 150px #000;');
+    closeVideo.setAttribute('style', 'display: block;');
+}
+
+img.addEventListener('click', setAttr);
+closeVideo.addEventListener('click', close);
+showVideo.addEventListener('click', show);
